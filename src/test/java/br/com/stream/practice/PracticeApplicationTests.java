74f0b84c88451e787a5779a3f4674b5bc06dd41e
fabricio <fabricio.jacob@outlook.com>
2020-03-28 21:40:25 -0300

    init project.

---
+++ src/test/java/br/com/stream/practice/PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.stream.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
